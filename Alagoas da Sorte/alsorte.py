d37cf93760917e8c3edb64a8e99c6c11e16a993b
unknown <victor.costa.23a@gmail.com>
2018-04-20 19:10:58 -0300

    Melhoria de código alsorte.py

--- Alagoas da Sorte/alsorte.py
+++ Alagoas da Sorte/alsorte.py
@@ -16,13 +16,26 @@ for response in system:
 	for rt in rts:
 		tr = rt.get_text()
 		l.append(tr)
-
+lst = []
 for i in range(1, 10):
 	a = '0' + str(i)
 	c = 'O numero ' + a + ' = '
-	print(c + str(l.count(a)))
+	t = c + str(l.count(a))
+	lst.append(t)
 
 for i in range(10, 61):
 	a = str(i)
 	c = 'O numero ' + a + ' = '
-	print(c + str(l.count(a)))
+	s = c + str(l.count(a))
+	lst.append(s)
+
+arq = open('padroes.txt', 'w')
+for i in range(0, len(lst)):
+	if i == 6 or i == 12 or i == 18 or i == 24 or i == 30 or i == 36 or i == 42 or i == 48 or i == 54:
+		arq.write('\n\n')
+
+	arq.writelines(lst[i])
+	arq.write('     ')
+arq.close()
+		
+	
