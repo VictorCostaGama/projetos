8f443d4380ab95c36ee22eff47fb39b753971558
unknown <victor.costa.23a@gmail.com>
2018-04-24 15:13:24 -0300

    Adicionado alst.py

---
+++ Alagoas da Sorte/alst.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+
+def informacao(soup, l_g):
+
+	system = soup.find_all('div', id='noshow')
+
+	l = []
+
+	for response in system:
+		rts = response.find_all('td')
+		for rt in rts:
+			tr = rt.get_text()
+			l.append(tr)
+			l_g.append(tr)
+
+	lst = []
+
+	for i in range(1, 10):
+		a = '0' + str(i)
+		c = 'O numero ' + a + ' = '
+		t = c + str(l.count(a))
+		lst.append(t)
+		
+	for i in range(10, 61):
+		a = str(i)
+		c = 'O numero ' + a + ' = '
+		s = c + str(l.count(a))
+		lst.append(s)
+        
+	stre = soup.find_all('div', id='resultados_edicao')
+
+	for ert in stre:
+		title = ert.p.get_text()
+
+	arq = open('Padroes.txt', 'a')
+	arq.write('|-------------------------------------------------------------------------------------------------------------------|')
+	arq.write('\n|                                                                                                                   |\n|                                   TABELA '+ title + '                                            |')
+	arq.write('\n|                                                                                                                   |\n|-------------------------------------------------------------------------------------------------------------------|\n')
+	arq.write('|                                                                                                                   |\n|                                                                                                                   |\n')
+	for i in range(0, len(lst)):
+		if i == 0:
+			arq.write('|')
+		if i == 5 or i == 10 or i == 15 or i == 20 or i == 25 or i == 30 or i == 35 or i == 40 or i == 45 or i == 50 or i == 55 or i == 60 or i == 61:
+			arq.write('|\n|                                                                                                                   |\n|')
+
+		arq.writelines('    ' + lst[i])
+		arq.write('    ')
+		
+	arq.write('|\n|                                                                                                                   |\n|-------------------------------------------------------------------------------------------------------------------|\n\n\n\n')
+	arq.close()
+
+	l.clear()
+	lst.clear()
+
+	return l_g
