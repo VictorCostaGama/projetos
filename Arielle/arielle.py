399bed1bd51565cf1047f717abfc6fcbbcd90537
VictorCostaGama <victor.costa.23a@gmail.com>
2018-04-26 16:09:21 -0300

    Adicionado arquivo arielle.py

---
+++ Arielle/arielle.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'http://www.arielle.com.br/produtos/listar?infinite=true'
+
+lista_url_produto = []
+
+for i in range(1, 7):
+
+    parm = {'pag':str(i)}
+
+    r = requests.get(url, params=parm)
+
+    soup = BeautifulSoup(r.text, 'lxml')
+
+    url_prod = soup.find_all('div', class_='produto')
+    
+    for url_p in url_prod:
+        url_produto = url_p.a.get('href')
+        lista_url_produto.append(url_produto)
+
+print(lista_url_produto)
